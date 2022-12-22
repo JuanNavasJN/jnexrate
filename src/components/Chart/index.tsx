24dc0f0a10b02b761733c705c574280a325072f6
Juan Navas <iam@juannavas.dev>
2022-12-22 13:04:31 -0400

    Add tradingview

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
+
 import './Chart.css';
 
 const Chart: React.FunctionComponent = () => {
+  const [symbol] = useState('EURUSD');
+
   return (
     <section id="chart-section">
-      <div id="chart"></div>
+      <div id="chart">
+        <AdvancedRealTimeChart
+          symbol={symbol}
+          theme="dark"
+          autosize
+          hide_side_toolbar
+          hide_top_toolbar
+          style="3"
+        ></AdvancedRealTimeChart>
+      </div>
     </section>
   );
 };
