82ec92caf4f76c774f07418fd03ae15f7e4bf8e5
Juan Navas <iam@juannavas.dev>
2022-12-24 08:55:58 -0400

    Refactor pair context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
-import { useState } from 'react';
 import Form from './components/Form';
 import Chart from './components/Chart';
-import { PairContext } from './contexts';
-import { PAIR_OPTIONS } from './util/constants';
-
-const initialPair = PAIR_OPTIONS[0].value;
+import PairProvider from './contexts/PairProvider';
 
 const App: React.FunctionComponent = () => {
-  const [pair, setPair] = useState(initialPair);
-  const [currency1, setCurrency1] = useState(initialPair.substring(0, 3));
-  const [currency2, setCurrency2] = useState(initialPair.substring(3, 6));
-
-  const setCurrencies = (pair: string) => {
-    setCurrency1(pair.substring(0, 3));
-    setCurrency2(pair.substring(3, 6));
-  };
-
-  const pairContextValue = {
-    pair,
-    currency1,
-    currency2,
-    rate: 0,
-    setPair: (pair: string) => {
-      setPair(pair);
-      if (pair.includes(':')) {
-        setCurrencies(pair.split(':')[1]);
-      } else {
-        setCurrencies(pair);
-      }
-    }
-  };
-
   return (
-    <PairContext.Provider value={pairContextValue}>
+    <PairProvider>
       <div id="app">
         <h1>JNExRate</h1>
 
@@ -41,7 +13,7 @@ const App: React.FunctionComponent = () => {
           <Chart />
         </main>
       </div>
-    </PairContext.Provider>
+    </PairProvider>
   );
 };
 
---
+++ src/contexts/PairProvider.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { PAIR_OPTIONS } from '../util/constants';
+import { PairContext } from './';
+
+const initialPair = PAIR_OPTIONS[0].value;
+
+interface PairProviderProps {
+  children?: React.ReactNode;
+}
+
+const PairProvider: React.FunctionComponent<PairProviderProps> = ({
+  children
+}) => {
+  const [pair, setPair] = useState(initialPair);
+  const [currency1, setCurrency1] = useState(initialPair.substring(0, 3));
+  const [currency2, setCurrency2] = useState(initialPair.substring(3, 6));
+
+  const setCurrencies = (pair: string) => {
+    setCurrency1(pair.substring(0, 3));
+    setCurrency2(pair.substring(3, 6));
+  };
+
+  const pairContextValue = {
+    pair,
+    currency1,
+    currency2,
+    rate: 0,
+    setPair: (pair: string) => {
+      setPair(pair);
+      if (pair.includes(':')) {
+        setCurrencies(pair.split(':')[1]);
+      } else {
+        setCurrencies(pair);
+      }
+    }
+  };
+
+  return (
+    <PairContext.Provider value={pairContextValue}>
+      {children}
+    </PairContext.Provider>
+  );
+};
+
+export default PairProvider;
